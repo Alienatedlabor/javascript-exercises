4f881b988eba6c012ddd60fb391ebb9dbb3b1d94
Cole <propertyisrobbery@hotmail.com>
2022-05-21 19:59:46 -0400

    finish exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+function reverseString(string) {
+ return string.split("").reverse().join("");
 };
+  reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+let array = [1, 2, 3, 4]
+const removeFromArray = function(array) {
+ 
+  array.splice(2, 1);
+  return array;
 };
-
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
