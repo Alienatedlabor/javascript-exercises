b1e9ad8a02fe98702d7b38940e8f60de43b6856e
Cole <propertyisrobbery@hotmail.com>
2022-05-25 18:33:28 -0400

    finish temp conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(farenheitInput) {
+let celsiusResult = (farenheitInput - 32) * 5/9;
+celsiusResult = Math.round(celsiusResult * 10) / 10;
+return celsiusResult;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusInput) {
+let farenheitResult = (celsiusInput * 9/5) + 32;
+farenheitResult = Math.round(farenheitResult * 10) / 10;
+return farenheitResult;
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
