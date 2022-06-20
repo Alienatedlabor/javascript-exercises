d76f650a3564fdf2ff63156fbadff0f747ea23ba
Cole <propertyisrobbery@hotmail.com>
2022-06-20 17:36:00 -0400

    solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...Numbers]) {
+  let total = 0;
+  for (const number of Numbers) {
+    total += number;
+  }
+  return ~~[total];
+};
+// using .reduce:
+// const sum = function(array){
+// return array.reduce((total,current) => total + current, 0)
+// }
+// 0 at the end is start total
+
+const multiply = function (array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const multiply = function() {
-
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function factorial(number) {
+  if (number < 0){
+    return -1;
+  } else if (number == 0){
+    return 1;
+  } else return number * factorial(number - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
