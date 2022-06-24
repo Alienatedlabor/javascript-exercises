27f634c535d6a45a23ea0d92283d26fdd35f39d9
Cole <propertyisrobbery@hotmail.com>
2022-06-24 19:59:35 -0400

    solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let modifiedWord = word.toLowerCase().replace(/([^a-z])/g, "");
+  let reverse = modifiedWord.split("").reverse().join("");
+  if (reverse === modifiedWord) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+// if (word.length > 2) {
+//  reverse = word.split(" ").reverse().join("");
+//   if (word === reverse) {
+//     return true;
+//   }
 // Do not edit below this line
 module.exports = palindromes;
+// let reverse = word
+// .toLowerCase()
+// .split("")
+// .reverse()
+// .join("")
+// .split(" ")
+// .join();
+// if (word === reverse) {
+// return true;
+// } else {
+// return false;
+// }
