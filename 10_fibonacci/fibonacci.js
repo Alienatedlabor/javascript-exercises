d7c0579cbc64bffb3a68547fefa757a3844c8425
Cole <propertyisrobbery@hotmail.com>
2022-06-24 21:07:27 -0400

    solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (parameter) {
+  let fn1 = 0;
+  let fn2 = 1;
+  let fntotal = ~~parameter;
+  if (fntotal < 0) {
+    return "OOPS";
+  }
+  for (let i = 2; i <= parameter; i++) {
+    fntotal = fn1 + fn2;
+    fn1 = fn2;
+    fn2 = fntotal;
+  }
+  return fntotal;
 };
 
 // Do not edit below this line
